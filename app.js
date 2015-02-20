4650090d7f2d77c765e0e53728c1e948ae669d2a
Andrew McGill <arm5077@gmail.com>
2015-02-19 20:17:09 -0500

    WHOLE LOTTA CHANGES

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ app.controller("quizController", ["$scope", "$http", "$sce", function($scope, $h
 			
 		});
 	
+	$scope.evaluateAnswer = function(answer, question){
+		if( answer.correct == true ) {
+			answer.right = true;
+			question.correct = false;
+		}
+		else {
+			answer.right = false;
+			question.correct = false;
+		}
+		
 	
+		
+	}
 	
 }]);
+
+app.directive('image', function() {
+    return { 
+      link: function(scope, element, attributes){
+        element.css("height", (element[0].offsetWidth / 1.618) + "px");
+		
+		setInterval(function(){
+			element.css("height", (element[0].offsetWidth / 1.618) + "px");
+		}, 1000);
+
+      }
+    }
+});
