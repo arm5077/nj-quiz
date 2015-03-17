1dc321a755788745ba16e8be563553fd46fb3256
Andrew McGill <arm5077@gmail.com>
2015-03-17 12:46:46 -0400

    back to normal

--- public/embed-full.js
+++ public/embed-full.js
@@ -2,17 +2,17 @@
 
 console.log("Embedded scripts running ...");
 
-var pymParent = new pym.Parent('pym', "http://nj-quiz.herokuapp.com/quiz/?id=DChistory", {});
+var pymParent = new pym.Parent('pym', document.getElementById('pym').className, {});
 
 resize();
 $(window).resize(resize);
-
+setInterval(resize, 1000);
 
 function resize(){
 	if( $(window).width() > 1120 ){
 		$("#pym").css({
-			"width": "180%",
-			"margin-left": "-40%"
+			"width": "115%",
+			"margin-left": "-7%"
 		});
 	}
 	else {
